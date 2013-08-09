0c7c03d4893f7523c937f89169639568edd67920
Sebastiaan Hoejenbos <sebastiaan.hoejenbos@gmail.com>
2013-08-09 11:00:36 +0200

    dynamic port

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
   , fs = require('fs')
 
-app.listen(80);
+// Configure the port to run in the Windows Azure emulator
+var port = process.env.port || 81;
+// start listening for HTTP connections
+app.listen(port);
+
+console.log('listening on port:' + port);
 
 function handler (req, res) {
   fs.readFile(__dirname + '/index.html',
